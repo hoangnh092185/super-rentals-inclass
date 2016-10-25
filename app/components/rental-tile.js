7436d3c36e481599b2d888b8a0399df8870991fe
David Ayala and Nhat Hoang <david@>
2016-10-25 09:09:52 -0700

    add rental detail router/handlebar and move update and delete function from index to rental js file.

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -9,13 +9,13 @@ export default Ember.Component.extend({
     imageHide: function(){
       this.set('isImageShowing', false);
     },
-    update(rental, params){
-      this.sendAction('update', rental, params);
-    },
-    delete(rental){
-      if(confirm('Are you sure you want to delete this rentals?')){
-        this.sendAction('destroyRental', rental);
-      }
+    // update(rental, params){
+    //   this.sendAction('update', rental, params);
+    // },
+    // delete(rental){
+    //   if(confirm('Are you sure you want to delete this rentals?')){
+    //     this.sendAction('destroyRental', rental);
+    //   }
     }
   }
 });
